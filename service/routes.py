3757d23add30c8a13351a3e5eb1e179ac479f7b5
HannaVar1 <hanna.varavva@gmail.com>
2026-04-01 09:55:09 -0400

    add tests for read feature

--- service/routes.py
+++ service/routes.py
@@ -68,8 +68,12 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
-
+@app.route("/accounts/<id>", methods=["GET"])
+def read_account(id):
+    resp  = Account.find(id)
+    if not resp:
+        abort(status.HTTP_404_NOT_FOUND,f"Account with id [{id}] could not be found.")
+    return resp.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
